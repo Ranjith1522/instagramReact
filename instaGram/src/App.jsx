08970b45f39791abcda1ce8bfe4ffc87ff318861
ranjith <ranjithkankati2@gmail.com>
2025-10-24 20:58:57 +0530

    workaround added kite-zerodha login

--- instaGram/src/App.jsx
+++ instaGram/src/App.jsx
@@ -4,25 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div>
-      <div>
-        
+      <div className="login-container">
+        <img src='https://kite.zerodha.com/static/images/kite-logo.svg'></img>
+        <h3>Login to Kite</h3>
+        <input placeholder='Phone or User ID'></input>
+        <input type='password' placeholder='Password'></input><br></br>
+        <button className='login'>Login</button>
+        <p>Forgot user ID or Password?</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
