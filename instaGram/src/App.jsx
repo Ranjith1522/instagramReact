ce548f12d1222eea33fb6e9d3b8452a8862bb7b3
ranjith <ranjithkankati2@gmail.com>
2025-10-26 22:55:58 +0530

    added div tags

--- instaGram/src/App.jsx
+++ instaGram/src/App.jsx
@@ -4,17 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
       <div className="login-container">
-        <img src='https://kite.zerodha.com/static/images/kite-logo.svg'></img>
-        <h3>Login to Kite</h3>
-        <input placeholder='Phone or User ID'></input>
-        <input type='password' placeholder='Password'></input><br></br>
-        <button className='login'>Login</button>
-        <p>Forgot user ID or Password?</p>
+        <div>
+          <img src='https://kite.zerodha.com/static/images/kite-logo.svg'></img>
+        </div>
+        <div>
+          <h3>Login to Kite</h3>
+        </div>
+        <div>
+          <input placeholder='Phone or User ID'></input>
+        </div>
+        <div>
+          <input type='password' placeholder='Password'></input>
+        </div>
+        <div>
+          <button className='login'>Login</button>
+        </div>
+        <div>
+          <p>Forgot user ID or Password?</p>
+        </div>
+
       </div>
     </>
   )
